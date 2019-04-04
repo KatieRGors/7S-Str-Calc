5360b6c148e37bc5ef170279eb96f0d78d2ddeba
Kaitlyn Gorsalitz <Katie@kaitlyns-mbp-2.gateway.sasktel.net>
2019-04-03 18:50:22 -0600

    Added tests for objects that are not strings

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -5,3 +5,18 @@ def test_empty_string():
     expected_output = 0
     actual_output = StringCalculator().Add(input_text)
     assert expected_output == actual_output
+
+
+def test_none_instead_of_string():
+    input_text = None
+    expected_output = 0
+    actual_output = StringCalculator().Add(input_text)
+    assert expected_output == actual_output
+
+
+def test_integer_instead_of_string():
+    input_text = 1
+    expected_output = 0
+    actual_output = StringCalculator().Add(input_text)
+    assert expected_output == actual_output
+
