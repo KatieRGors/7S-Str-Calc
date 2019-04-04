657cc5c911e41ab311e7f771a70847e35f9f26ab
Kaitlyn Gorsalitz <Katie@kaitlyns-mbp-2.gateway.sasktel.net>
2019-04-03 20:11:51 -0600

    Implemented custom delimiters
    Finished custom delimiter parsing method with more testing

--- string_calculator.py
+++ string_calculator.py
@@ -1,15 +1,31 @@
+import re
+
+DEFAULT_DELIMITER = ','
+DELIMITER_DEFINITION_BEGINNING = '//'
+DELIMITER_DEFINITION_ENDING = '\n'
+DELIMITER_REGEX = r'^//[^\d\n/]\n'
+
+
 def get_delimiter(text):
     """
     Returns the delimiter parsed from given text. Delimiter is in the form
     //[delimiter]\n
     If no delimiter is found, the default delimiter is a comma
+    // and \n cannot be used as delimiters
     """
-    return ','
+    if isinstance(text, str):
+        result = re.search(DELIMITER_REGEX, text)
+        if result is not None:
+            sanitized_result = result.group()
+            sanitized_result = sanitized_result.replace(DELIMITER_DEFINITION_BEGINNING, '')
+            sanitized_result = sanitized_result.replace(DELIMITER_DEFINITION_ENDING, '')
+            return sanitized_result
+
+    return DEFAULT_DELIMITER
 
 
 class StringCalculator:
     """ A simple string calculator that performs operations on numbers in a string. """
-
 
     def Add(self, text):
         """ Adds together all delimited numbers in a string"""
@@ -17,12 +33,13 @@ class StringCalculator:
             return 0
 
         total = 0
-        sanitized_text = text.replace('\n', '')
-        numeric_list = sanitized_text.split(get_delimiter(text))
+        numeric_list = text.split(get_delimiter(text))
 
         # todo support negatives? floats?
         for element in numeric_list:
-            if element.isdigit() and int(element) <= 1000:
-                total += int(element)
+            sanitized_element = element.replace('\n', '')
+
+            if sanitized_element.isdigit() and int(sanitized_element) <= 1000:
+                total += int(sanitized_element)
 
         return total
--- tests/test_delimiters.py
+++ tests/test_delimiters.py
@@ -17,3 +17,39 @@ def test_delimiter_comma_string():
     input_text = ","
     expected_delimiter = ','
     assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_none_given():
+    input_text = "1,2,3"
+    expected_delimiter = ','
+    assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_semicolon_string():
+    input_text = "//;\n1;3;4"
+    expected_delimiter = ';'
+    assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_dollar_string():
+    input_text = "//$\n1$2$3"
+    expected_delimiter = '$'
+    assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_at_string():
+    input_text = "//@\n2@3@8"
+    expected_delimiter = '@'
+    assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_at_string_malformed_extra_slash():
+    input_text = "///@\n"
+    expected_delimiter = ','
+    assert expected_delimiter == get_delimiter(input_text)
+
+
+def test_delimiter_at_string_malformed_extra_newline():
+    input_text = "//\n@\n"
+    expected_delimiter = ','
+    assert expected_delimiter == get_delimiter(input_text)
